84f0f685907c4bb920a9415b89e39d690180f6eb
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-03-28 16:11:13 +0100

    add watch and data to watch

--- src/components/api-reference.vue
+++ src/components/api-reference.vue
@@ -9,6 +9,41 @@ export default {
   name: 'api-reference',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      a: 1,
+      b: 2,
+      c: {
+        d: 3,
+        e: 4
+      }
+    }
+  },
+  created() {
+    // top-level property name
+    this.$watch('a', (newVal, oldVal) => {
+      // do something
+    })
+
+    // function for watching a single nested property
+    this.$watch(
+      () => this.c.d,
+      (newVal, oldVal) => {
+        // do something
+      }
+    )
+
+    // function for watching a complex expression
+    this.$watch(
+      // every time the expression `this.a + this.b` yields a different result,
+      // the handler will be called. It's as if we were watching a computed
+      // property without defining the computed property itself
+      () => this.a + this.b,
+      (newVal, oldVal) => {
+        // do something
+      }
+    )
   }
 }
 </script>
